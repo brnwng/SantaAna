f1f0be3ec5e83f8bbdfc685082f2b81df98e85c6
Daniel <danielkyungpak@gmail.com>
2017-06-24 09:50:50 -0700

    initialize

---
+++ SantaAnaHackathon/SantaAna.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SantaAna.Web.Startup))]
+namespace SantaAna.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
